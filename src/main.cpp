0f9c22dbed5c1d440c457ca3344565b775e2c6b3
Glen Flethcer <glenflet@gmail.com>
2017-11-26 18:26:46 +1100

    getting-started Project 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include <SFML/Graphics.hpp>
 
+const sf::Vector2i size = {800, 600};
+const float radius = 10.0f;
+
 int main(int argc, char *argv[])
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "First SFML Program");
-    sf::CircleShape shape(10.0f);
+    sf::RenderWindow window(sf::VideoMode(size.x, size.y), "First SFML Program");
+    window.setVerticalSyncEnabled(true);
+    sf::Transform rotation;
+    sf::CircleShape shape(radius);
+    sf::Vector2f pos(size.x/2.0f - radius,size.y/2.0f - radius);
+    sf::Vector2f vec(5, 5);
+    rotation.rotate(1.5);
     shape.setFillColor(sf::Color::Blue);
-    shape.setPosition(400,250);
 
     while (window.isOpen())
     {
@@ -14,6 +21,12 @@ int main(int argc, char *argv[])
         {
             if (event.type == sf::Event::Closed) window.close();
         }
+
+        shape.setPosition(pos);
+        if (pos.x <= 0 and vec.x < 0 or pos.x >= size.x - 2*radius and vec.x > 0) vec.x = -vec.x;
+        if (pos.y <= 0 and vec.y < 0 or pos.y >= size.y - 2*radius and vec.y > 0) vec.y = -vec.y;
+        pos += vec;
+        vec = rotation.transformPoint(vec);
 
         window.clear();
         window.draw(shape);
